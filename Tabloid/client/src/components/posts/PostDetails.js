303bc8b7511df24c3235c61c6b25dd1ca30a70e4
Tiffany Baker <TAGesner@gmail.com>
2022-11-21 12:01:40 -0500

    going to merge main

--- Tabloid/client/src/components/posts/PostDetails.js
+++ Tabloid/client/src/components/posts/PostDetails.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Card, CardImg, CardBody } from "reactstrap";
 import { getPost } from "../../Managers/PostManager";
+import { useNavigate } from "react-router-dom";
 
 
 export const PostDetails = () => {
     const [post, setPost] = useState();
     const { id } = useParams();
+    const navigate = useNavigate();
 
     //all post image links are broken, so need to replace them all with a default image
     const handleBrokenImage = (image) => {
         const defaultImage = "https://contenthub-static.grammarly.com/blog/wp-content/uploads/2017/11/how-to-write-a-blog-post.jpeg";
         image.target.src = defaultImage;
     };
+    
     
     useEffect(() => {
         getPost(id).then(setPost);
@@ -30,7 +33,10 @@ export const PostDetails = () => {
                 <p>Author: {post.userProfile.displayName}</p>
             {/* </Link> */}
             <p>Published: {post.publishDateTime.substring(0, 10)}</p>
-            <button>Manage Tags</button>
+            <button onClick={(e) => {
+            navigate('/addTag')
+          }} style={{marginTop: '15px', width: '120px'}}
+          >Manage Tag</button>
             <CardImg top src={post.imageLocation} alt={post.title} onError={handleBrokenImage} />
             <p>{post.content}</p>
             
