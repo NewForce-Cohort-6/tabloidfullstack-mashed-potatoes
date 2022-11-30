f1cff58a91333f7ac955daea235e2fd611a25de0
Brandon Gray <bgizzle078@gmail.com>
2022-11-30 09:20:21 -0500

    New Main Commit

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -27,27 +27,6 @@ export default function ApplicationViews() {
 
   return (
     <Routes>
-
-        <Route path="/" element={<Hello />} />
-        <Route path="/tag" element={<Tag />} />
-        <Route path="/createTag" element={<TagForm />} />
-        <Route path="/deleteTag/:id" element={<TagDelete />} />
-        <Route path="/editTag/:id" element={<TagEdit />} />
-        <Route path="/users" element={<UserProfileList />} />
-        <Route path="/posts" element={ <PostList /> } />
-        <Route path="/myposts" element={ <MyPostList /> } />
-        <Route path="/createPost" element={ <PostForm /> } />
-        <Route path="/posts/:id" element={ <PostDetails /> } />
-        <Route path="/deletePost/:id" element={ <PostDelete /> } />        
-        <Route path="/editPost/:id" element={ <PostEdit /> } />        
-        <Route path="/categories" element={<CategoryList />} />
-        <Route path="/createCategory" element={<CategoryForm />} />
-        <Route path="/deleteCategory/:id" element={<CategoryDelete />} />
-        <Route path="/editCategory/:id" element={<CategoryEdit />} />
-        <Route path="*" element={<p>Whoops, nothing here...</p>} />
-          
-      </Routes>
-   );
  
       <Route path="/" element={<Hello />} />
       <Route path="/tag" element={<Tag />} />
@@ -60,6 +39,7 @@ export default function ApplicationViews() {
       <Route path="/createPost" element={<PostForm />} />
       <Route path="/posts/:id" element={<PostDetails />} />
       <Route path="/deletePost/:id" element={<PostDelete />} />
+      <Route path="/editPost/:id" element={ <PostEdit /> } />
       <Route path="/categories" element={<CategoryList />} />
       <Route path="/createCategory" element={<CategoryForm />} />
       <Route path="/deleteCategory/:id" element={<CategoryDelete />} />
