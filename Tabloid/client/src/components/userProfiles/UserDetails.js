4faeaaa2d216098a2a4c4573b891342d6f0ba7c0
CaraNetzer <cara.netzer@gmail.com>
2022-12-01 13:51:27 -0500

    started client side

--- Tabloid/client/src/components/userProfiles/UserDetails.js
+++ Tabloid/client/src/components/userProfiles/UserDetails.js
@@ -9,15 +9,21 @@ const UserProfileDetails = () => {
     const [user, setUser] = useState({})
     const {id} = useParams();
 
+    const localUser = localStorage.getItem("userProfile")
+    const userObject = JSON.parse(localUser)
+
     useEffect(
     () => {
         getUserById(id).then((u) => {setUser(u)})
     }, []);
 
+    const uploadImage = (e) => {
+        console.log(e.target.value);
+    }
+
     if (!user){
         return null;
     }
-console.log(user)
 
     return (
         <>
@@ -25,6 +31,14 @@ console.log(user)
             <div style={{borderBottom: '1px solid blue', width: '20%'}}>
             <h4>{user.displayName}</h4>
             <img src={user.imageLocation} />
+            {userObject.id == user.id
+                ? <> 
+                    <form action="/action_page.php">
+                        <input type="file" onChange={(e) => uploadImage(e)} id="myFile" name="filename"/>
+                    </form>
+                </>
+                : ""
+            }
             <h6 style={{marginTop: '15px'}}>Name: {user.firstName} {user.lastName}</h6>
             <h6>Email: {user.email}</h6>
             <p>UserType: {user?.userType?.name}
